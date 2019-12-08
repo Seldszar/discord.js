e7b2d34f82710bd88ee0a9595d62a2b6eb2256bc
Seldszar <contact@seldszar.fr>
2019-12-08 03:33:06 +0100

    fix(voice): ensure voice data interpolation

--- src/client/voice/VoiceConnection.js
+++ src/client/voice/VoiceConnection.js
@@ -12,10 +12,17 @@ const { OPCodes, VoiceOPCodes, VoiceStatus, Events } = require('../../util/Const
 const Speaking = require('../../util/Speaking');
 const Util = require('../../util/Util');
 
+// Required amount of frames of silence for ensuring voice data interpolation
+// https://discordapp.com/developers/docs/topics/voice-connections#voice-data-interpolation
+const SILENCE_FRAMES = 5;
+
 // Workaround for Discord now requiring silence to be sent before being able to receive audio
 class SingleSilence extends Silence {
   _read() {
-    super._read();
+    for (let index = 0; index < SILENCE_FRAMES; index++) {
+      super._read();
+    }
+
     this.push(null);
   }
 }
